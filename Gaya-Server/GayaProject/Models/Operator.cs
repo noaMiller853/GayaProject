5742c4fc2256ebc283bacee8cd921c5e91e50876
Noa Miller <128284076+noaMiller853@users.noreply.github.com>
2025-03-10 13:07:06 +0200

    fix file develop

--- Gaya-Server/GayaProject/Models/Operator.cs
+++ Gaya-Server/GayaProject/Models/Operator.cs
@@ -1,9 +1,12 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace WebApplicationUser.Models
 {
     public class Operator
     {
+        [Key]
         public int Id { get; set; }
         [Column(TypeName = "nvarchar(1)")]
         public string Operation { get; set; } = string.Empty;
